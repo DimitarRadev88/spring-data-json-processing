7bd9db28c436c3a8dee17e2f31f842df98893cd1
dimit <dimitar_rradev@abv.bg>
2024-12-11 12:27:58 +0200

    first commit

---
+++ src/test/java/bg/softuni/springdatajsonprocessing/SpringDataJsonProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.springdatajsonprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJsonProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
